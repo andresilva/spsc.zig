de58428448969bd7ac3fccddf744d6935e209895
André Silva <andrerfosilva@gmail.com>
2025-01-26 17:27:03 +0000

    add extra slot to ring and index directly

--- atomic.zig
+++ atomic.zig
@@ -2,8 +2,11 @@ const std = @import("std");
 
 pub fn Ring(comptime T: type, cap: usize) type {
     return struct {
-        // NOTE: all fields aligned to cache line to avoid false sharing
-        items: [capacity]T align(std.atomic.cache_line) = undefined,
+        // NOTE: all fields aligned to cache line to avoid false sharing.
+        // NOTE: we add an extra slot to the ring to be able to distinguish between
+        // an empty and full ring. the ring is considered empty when the head and tail
+        // are equal, and it's considered full when the head and tail are one slot apart.
+        items: [capacity + 1]T align(std.atomic.cache_line) = undefined,
         head: std.atomic.Value(usize) align(std.atomic.cache_line) = std.atomic.Value(usize).init(0),
         tail: std.atomic.Value(usize) align(std.atomic.cache_line) = std.atomic.Value(usize).init(0),
         cached_head: usize align(std.atomic.cache_line) = 0,
@@ -13,23 +16,37 @@ pub fn Ring(comptime T: type, cap: usize) type {
             std.debug.assert(@alignOf(@This()) == std.atomic.cache_line);
         }
 
-        pub const capacity = std.math.ceilPowerOfTwoAssert(usize, cap);
-        pub const mask = capacity - 1;
+        pub const capacity = cap;
 
         pub fn enqueue(self: *@This(), t: T) bool {
             const head = self.head.load(.unordered);
 
-            if (head - self.cached_tail == capacity) {
+            if (isFull(head, self.cached_tail)) {
                 self.cached_tail = self.tail.load(.acquire);
-                if (head - self.cached_tail == capacity) {
+                if (isFull(head, self.cached_tail)) {
                     return false;
                 }
             }
 
-            self.items[head & mask] = t;
-            self.head.store(head +% 1, .release);
+            self.items[head] = t;
+
+            if (head == capacity) {
+                self.head.store(0, .release);
+            } else {
+                self.head.store(head + 1, .release);
+            }
 
             return true;
+        }
+
+        fn isFull(head: usize, tail: usize) bool {
+            if (head < tail) {
+                return head == tail - 1;
+            } else if (tail < head) {
+                return tail == head - capacity;
+            } else {
+                return false;
+            }
         }
 
         pub fn dequeue(self: *@This()) ?T {
@@ -42,14 +59,19 @@ pub fn Ring(comptime T: type, cap: usize) type {
                 }
             }
 
-            const t = self.items[tail & mask];
-            self.tail.store(tail +% 1, .release);
+            const t = self.items[tail];
+
+            if (tail == capacity) {
+                self.tail.store(0, .release);
+            } else {
+                self.tail.store(tail + 1, .release);
+            }
 
             return t;
         }
 
         pub fn isEmpty(self: *@This()) bool {
-            return self.head.load(.seq_cst) == self.tail.load(.seq_cst);
+            return self.head.load(.unordered) == self.tail.load(.unordered);
         }
     };
 }

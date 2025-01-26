64085f4e82f2a34cf72e28d7250e03123746dd45
André Silva <andrerfosilva@gmail.com>
2025-01-26 17:38:08 +0000

    cleanup mutex ring implementation

--- mutex.zig
+++ mutex.zig
@@ -3,22 +3,22 @@ const std = @import("std");
 pub fn Ring(comptime T: type, cap: usize) type {
     return struct {
         items: [capacity]T = undefined,
-        head_index: usize = 0,
-        tail_index: usize = 0,
+        head: usize = 0,
+        tail: usize = 0,
         mutex: std.Thread.Mutex = .{},
 
         pub const capacity = cap;
 
-        pub fn enqueue(self: *@This(), t: T) bool {
+        pub fn enqueue(self: *@This(), item: T) bool {
             self.mutex.lock();
             defer self.mutex.unlock();
 
-            if (self.len() == capacity) {
+            if (self.head -% self.tail == capacity) {
                 return false;
             }
 
-            self.items[self.head_index % capacity] = t;
-            self.head_index += 1;
+            self.items[self.head % capacity] = item;
+            self.head +%= 1;
 
             return true;
         }
@@ -27,25 +27,28 @@ pub fn Ring(comptime T: type, cap: usize) type {
             self.mutex.lock();
             defer self.mutex.unlock();
 
-            if (self.len() == 0) {
+            if (self.head == self.tail) {
                 return null;
             }
 
-            const t = self.items[self.tail_index % capacity];
-            self.tail_index += 1;
+            const item = self.items[self.tail % capacity];
+            self.tail +%= 1;
 
-            return t;
-        }
-
-        fn len(self: *@This()) usize {
-            return self.head_index - self.tail_index;
+            return item;
         }
 
         pub fn isEmpty(self: *@This()) bool {
             self.mutex.lock();
             defer self.mutex.unlock();
 
-            return self.len() == 0;
+            return self.head == self.tail;
+        }
+
+        pub fn isFull(self: *@This()) bool {
+            self.mutex.lock();
+            defer self.mutex.unlock();
+
+            return self.head -% self.tail == capacity;
         }
     };
 }
